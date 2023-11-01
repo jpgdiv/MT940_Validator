cf61be53b7fdde4b1ddb3343bc2666056baecca5
jpgdiv <peter@divotion.com>
2023-11-01 14:36:13 +0100

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <title>Record Validator</title>
+      <Typography variant="h4">Record Validator</Typography>
     </div>
   );
 }
---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+import { blue, orange } from "@mui/material/colors";
+
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: orange[500],
+		},
+		secondary: {
+			main: blue[900],
+		},
+	},
+});
+
+export default theme
